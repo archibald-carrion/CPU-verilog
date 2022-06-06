45a50dd67468a247ee54e7bef2fa4a5c7620cb8c
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-06-06 14:41:41 -0600

    inicial comment registros.v file

--- registros.v
+++ registros.v
@@ -1,3 +1,7 @@
-/*********************************
+/*******************************
+Universidad de Costa Rica
+CI-0114: Fundamentos de Arquitectura
 
-*****************************
+Circuito simple de registro que permite almacenar 32 bits
+*****************************/
+
