58c610bdf801f9c696b30774a43706b7d6e27a37
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-06-13 09:35:05 -0600

    arquitecture error in register
    the registros circuit must represent an array of 8 registers, there for i need to change all the code of these file

--- registros.v
+++ registros.v
@@ -5,19 +5,24 @@ CI-0114: Fundamentos de Arquitectura
 Circuito simple de registro que permite almacenar 32 bits
 *****************************/
 
-module memoryRegister(dataOutput, dirrOutput, dataInput);
-	parameter BITS_DATA = 32;
-	parameter BITS_ADDR = 16;
+module registersArray();
+	//parameter BITS_DATA = 32;
+	//parameter BITS_ADDR = 16;
 	
-	output reg [BITS_DATA-1:0] dataOutput;
-	output reg [BITS_ADDR-1:0] dirrOutput;
-	input [BITS_DATA-1:0] dataInput;
+	//output [BITS_DATA-1:0] dataOutput;
+	//wire dataOutput;
+	//output [BITS_ADDR-1:0] dirrOutput;
+	//wire dirrOutput;
+	//input [BITS_DATA-1:0] dataInput;
+	//wire dataInput;
+//	reg dataInput, dataOutput, dirrOutput;
 	
-	reg [BITS_DATA-1:0] contentRegister;
+	//reg [BITS_DATA-1:0] contentRegister;
 	
-	always @ (dataInput) begin
-		contentRegister = dataInput;
-		dataOutput = contentRegister;
-	end
+	//always @ (dataInput) begin
+		//contentRegister = dataInput;
+		//dirrOutput = contentRegister[16:BITS_DATA-1];
+		//dataOutput = dataInput;
+//	end
 	
 endmodule
--- registros_tb.v
+++ registros_tb.v
@@ -8,12 +8,12 @@ Circuito de prueba para el circuito registro.v
 `timescale 1us/100ns
 
 module Register_tb;
-	wire [31:0] data_Output;
-	reg  [31:0] data_Input;
-	wire  [15:0] data_Dirrecion;
+	//reg [31:0] data_Output;
+	//reg  [31:0] data_Input;
+	//reg  [15:0] data_Dirrecion;
   
 	initial begin
-		$dumpfile("Registro.vcd");
+		$dumpfile("registros.vcd");
 		$dumpvars;
 		// en 1 registor cabe 32bits, lo cual son 4 bytes, y por lo tanto 4 char
 		
@@ -22,14 +22,16 @@ module Register_tb;
 		//#1 direccion = 5;
 		//#2 data_entrada = 'hACED_CAFE; direccion = 4; escribir = 1;
 		//#1 data_Input <= 'hHOLA;  
-		#1 data_Input = 'hACED_CAFE;
-		#1 data_Input = 'hDEAD_BEEF;
+		//#1 data_Input = 'hACED_CAFE;
+		//#1 data_Input = 'hDEAD_BEEF;
 		#1 $finish;
 		//#1 escribir = 0;
 		//#1 direccion = 3; data_entrada = 'hDEAD_BEEF;
 		//#6 $finish;
 	end
 	
-	memoryRegister registro(.dataOutput(data_Output), .dirrOutput(data_Dirrecion), .dataInput(data_Input));
+	registersArray registro();
 	//Mem_D32b_A16b mem(.data_out(data_salida),.data_in(data_entrada),.address(direccion),.write(escribir),.clk(clk));
 endmodule
+
+
