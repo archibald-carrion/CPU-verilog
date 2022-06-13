bc3575a326fe5fdfcc382ac45f66b398a8d988ea
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-06-13 11:41:35 -0600

    dev registros.v

--- registros.v
+++ registros.v
@@ -9,9 +9,23 @@ module registersArray(inputData, outputData1, outputData2, dirrInput, dirrOutput
 	parameter BITS_DATA = 32;	//tamano de los registros
 	parameter BITS_ADDR = 3;	//dirrecion que permite saber con cual registro trabajar
 	
+	input [BITS_DATA-1:0] inputData;
+	input [BITS_ADDR-1:0] dirrInput;
+	output [BITS_DATA-1:0] outputData1;
+	output [BITS_DATA-1:0] outputData2;
+	output [BITS_ADDR-1:0] dirrOutput1;
+	output [BITS_ADDR-1:0] dirrOutput2;
 	
+	reg [BITS_DATA-1:0] R0;
+	reg [BITS_DATA-1:0] R1;
+	reg [BITS_DATA-1:0] R2;
+	reg [BITS_DATA-1:0] R3;
+	reg [BITS_DATA-1:0] R4;
+	reg [BITS_DATA-1:0] R5;
+	reg [BITS_DATA-1:0] R6;
+	reg [BITS_DATA-1:0] R7;
 	
-	
+	//WE WILL USE A SWITCH FOR EVERY CASE OF DIRR OUTPUT
 	
 	
 	//parameter BITS_DATA = 32;
