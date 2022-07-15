84716b3c7b80cd13aca178dad3f28b827628c945
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-07-15 09:10:41 -0600

    se arreglasron bugs de ejecucion

--- cpu.v
+++ cpu.v
@@ -10,15 +10,7 @@ Ancho de palabra (Datos) 32 bits, ancho de direcciones 16 bits (64Ki Words)
 iverilog alu.v cpu.v memoria.v registros.v cpu_tb.v
 *********************************/
 
-
-// iverilog ejemplo_and_or.v ejemplo_and_or_tb.v
-// vvp a.out
-// gtkwave ejemplo_and_or.vcd
-
-
-
 `include "opcodes.vh"
-
 `define STAGE_FE_0 0	// fetch 0
 `define STAGE_FE_1 1	// fetch 1
 `define STAGE_DE_0 2	// decode 0
@@ -30,6 +22,8 @@ iverilog alu.v cpu.v memoria.v registros.v cpu_tb.v
 `define STAGE_WB_0 8	// write back 0
 `define STAGE_WB_1 9	// write back 1
 `define STAGE_HLT  10	// halt
+
+
 
 module CPU(MBR_W, write, MAR, MBR_R, reset, clk);
 	
@@ -78,10 +72,6 @@ module CPU(MBR_W, write, MAR, MBR_R, reset, clk);
 
 	reg [3:0] stage;
   reg [4:0] opcodeReduced;
-	//reg C;
-	//reg S;
-	//reg O;
-	//reg Z;
 	
 	//se ejecuta la maquina de estado durante los posedge del reloj
 	always @(posedge clk or reset) begin
@@ -91,16 +81,23 @@ module CPU(MBR_W, write, MAR, MBR_R, reset, clk);
 		end 
     else begin
 			case (stage)
+
+        //############################################################################################
+
 				`STAGE_FE_0: begin
 					stage <= `STAGE_FE_1;
 					MAR   <= PC;
 				end
 
+        //############################################################################################
+
 				`STAGE_FE_1: begin
 					stage <= `STAGE_DE_0;
-					PC <= PC + 1;				//se incrementa el PC para poder hacer fetch de la instruction siguiente en el proximo ciclo
+					PC <= PC + 1;				//se incrementa PC para hacer fetch de la instruction siguiente en el proximo ciclo
 					IR <= MBR_R;
 				end
+
+        //############################################################################################
 
 				`STAGE_DE_0: begin
 					stage <= `STAGE_DE_1;
@@ -130,6 +127,8 @@ module CPU(MBR_W, write, MAR, MBR_R, reset, clk);
 					//DECODE 0
 				end
 
+        //############################################################################################
+
 				`STAGE_DE_1: begin
 					stage <= `STAGE_EX_0;
           // Si desea realizar un load de registro a registro
@@ -140,12 +139,14 @@ module CPU(MBR_W, write, MAR, MBR_R, reset, clk);
           end			
 					//DECODE 1
 				end
+
+        //############################################################################################
 				
 				`STAGE_EX_0: begin
           // Si es una operacion de la ALU
 					if(opcodeReduced>=16 && opcodeReduced<=25) begin
             // Guarde el resultado en un buffer
-            resultadoReg = resultado;
+            resultadoReg <= resultado;
             // Vaya directo a Write-Back (WB)
             stage <= `STAGE_WB_0;
 					end 
@@ -153,15 +154,17 @@ module CPU(MBR_W, write, MAR, MBR_R, reset, clk);
             // Si la instruccion es un salto
             if (opcode == 209) begin
               // guarde el primer valor para comparar
-              salidaRegistrosReg01 = salidaRegistros;
+              salidaRegistrosReg01 <= salidaRegistros;
 
               // Lea el otro valor
-              addressRegistrosLectura = dst;
-              writeRegistros = 0;
+              addressRegistrosLectura <= dst;
+              writeRegistros <= 0;
             end
             stage <= `STAGE_EX_1;
           end
 				end
+
+        //############################################################################################
 
 				`STAGE_EX_1: begin
           // Si es un Load Directo o un store (usa memoria)
@@ -174,7 +177,7 @@ module CPU(MBR_W, write, MAR, MBR_R, reset, clk);
             // Si la instruccion es un salto
             if (opcode == 209) begin
               // guarde el segundo valor para comparar
-              salidaRegistrosReg02 = salidaRegistros;
+              salidaRegistrosReg02 <= salidaRegistros;
               
               // Si la condicion de salto se cumple
               if (salidaRegistrosReg01 != salidaRegistrosReg02) begin
@@ -207,13 +210,15 @@ module CPU(MBR_W, write, MAR, MBR_R, reset, clk);
 
 				end
 
+        //############################################################################################
+
 				`STAGE_MA_0: begin	
 				  stage <= `STAGE_MA_1;
           // Si estamos haciendo un store
           if (opcodeReduced == 2) begin
             // Guardamos el valor de registros que queremos
             // escribir en memoria
-            salidaRegistrosReg01 = salidaRegistros;
+            salidaRegistrosReg01 <= salidaRegistros;
             // Habilitamos escritura
             //writeMemoria = 1;
             write <= 1;
@@ -226,70 +231,75 @@ module CPU(MBR_W, write, MAR, MBR_R, reset, clk);
           end
 				end
 
+        //############################################################################################
+
 				`STAGE_MA_1: begin	
           // Si estamos haciendo un store
           if (opcodeReduced == 2) begin
             // Cambiamos el valor de escritura
-            entradaMemoria = salidaRegistrosReg01;
-            MBR_W = salidaRegistrosReg01;
+            //entradaMemoria = salidaRegistrosReg01;
+            MBR_W <= salidaRegistrosReg01;
             // Ya has terminado tu funcion
             stage <= `STAGE_FE_0;
           end
           // Si estamos haciendo un Load directo
           else begin
             // Guardamos el valor de la memoria
-            salidaMemoriaReg = salidaMemoria;
+            //salidaMemoriaReg = salidaMemoria;
+            //salidaMemoriaReg <= MBR_R;
             // Continuar con escritura en registros
             stage <= `STAGE_WB_0;
           end
-
 				end
+
+        //############################################################################################
 
 				`STAGE_WB_0: begin
           stage <= `STAGE_WB_1;
           case (opcode)
             `OPD_LD_INM: begin
+              // se guarda en un registro el contenido de una celda de memoria de la cual 
+              // tenemos el address
             end
             `OPD_LD_REG: begin
-              // Guardamos el valor del registro src
-              salidaRegistrosReg01 = salidaRegistros;
-              // Cambiamos el valor de escritura al valor de src
-              entradaRegistros = salidaRegistrosReg01;
+              salidaRegistrosReg01 <= salidaRegistros;  // Guardamos el valor del registro src
+              entradaRegistros <= salidaRegistrosReg01; // Cambiamos el valor de escritura al valor de src
             end
             `OPD_LD_DIRECT: begin
               // Cambiamos el valor de escritura al valor de la memoria
-              entradaRegistros = salidaMemoriaReg;
+              //entradaRegistros <= salidaMemoriaReg;
+              //entradaRegistros <= MBR_R;
+              entradaRegistros <= operandoB;
             end
             // Casos de opcode para Alu
             default: begin
               // Cambiamos el valor de escritura al valor obtenido de la Alu
-              entradaRegistros = resultadoReg;
+              entradaRegistros <= resultadoReg;
             end
           endcase;
 				end
 
+        //############################################################################################
+
 				`STAGE_WB_1: begin
           stage <= `STAGE_FE_0;
-          // Escribimos el valor en los registros
-          writeRegistros = 1;
+          writeRegistros <= 1;    // Escribimos el valor en los registros
 				end
+
+        //############################################################################################
 
 				default: begin
 					stage <= `STAGE_HLT;
 				end
+
+        //############################################################################################
+
 			endcase
 		end
 	end
 
 ALU alu(resultado, C, S, O, Z, operandoA, operandoB, opcodeReduced);
 
-/*
-Mem_D32b_A16b mem(salidaMemoria, 					   	// output de la memoria
-						entradaMemoria,   						    // input de la memoria
-						addressMemoria,						        // address de memoria de la celda que se quiere leer
-						writeMemoria,									    // write = 0, ya que queremos leer la memoria y no guardar nada
-						clk);									            // clk en 1, ya que la escritura se ejecuta en clk = 0
-*/
 registersArray registros(.inputData(entradaRegistros), 
 											 .dirrInput(addressRegistrosEscritura), 
 											 .dirrOutput1(addressRegistrosLectura), 
